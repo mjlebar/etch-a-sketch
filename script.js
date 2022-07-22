123d25d8800e10d4e046eec2472c6009a89e0431
mjlebar <106131602+mjlebar@users.noreply.github.com>
2022-07-22 12:28:48 -0500

    Added ability to change number of squares in grid

--- script.js
+++ script.js
@@ -2,20 +2,47 @@
 
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
-console.log(button);
-
 const squares = [];
 
 for (let i = 0; i < 256; i++) {
-  console.log("in");
   const newSquare = document.createElement("div");
   newSquare.classList.add("grid");
   container.appendChild(newSquare);
   squares.push(newSquare);
 }
 
-for (const square of squares) {
-  square.addEventListener("mouseover", (e) =>
-    e.target.classList.add("hovered")
-  );
+function addListeners(squares) {
+  for (const square of squares) {
+    square.addEventListener("mouseover", (e) =>
+      e.target.classList.add("hovered")
+    );
+  }
+}
+
+addListeners(squares);
+
+button.addEventListener("click", newGrid);
+
+function newGrid() {
+  const dim = prompt("How many squares per side do you want the grid to have?");
+  //   console.log(dim);
+
+  if (dim > 100) {
+    alert("Sorry, that is too big! Please pick a number no greater than 100");
+    return;
+  }
+
+  for (const square of squares) {
+    square.remove();
+  }
+  for (let i = 0; i < dim ** 2; i++) {
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("grid");
+    container.appendChild(newSquare);
+    squares.push(newSquare);
+    console.log(576 / dim);
+    newSquare.style.height = `${576 / dim - 2}px`;
+    newSquare.style.width = newSquare.style.height;
+  }
+  addListeners(squares);
 }
